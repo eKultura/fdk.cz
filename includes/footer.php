8998352e942baf957b29f4bb97b4319cd1501e09
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-05 20:30:04 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>O nás</h4>
+                    <p>Lorem ipsum dolor sit amet. Ano, tento text tu chceme mít. Je to symbol něčeho co vzniká a mění se. </p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Užitečné odkazy</h4>
+                    <ul class="footer-links">
+                        <li><a href="https://fdk.cz">Domů</a></li>
+                        <li><a href="#">O nás</a></li>
+                        <li><a href="#">Služby</a></li>
+                        <li><a href="#">Kontakt</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Kontaktuje nás:</h4> 
+                    <p>eKultura z.s.</p>
+                    <p>Email: info@div.cz</p>
+                    <p>Phone: hmm, uvidíme</p>
+                </div>
+            </div>
+        </div>
+    </footer>
