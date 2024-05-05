21c5213cc203aea3fb0b8753b13f9d03bf93b823
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-05 21:22:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
-require_once "include/db_connetion.php";
+require_once "includes/db_connection.php";
 include "includes/settings.php";
 
 $translations = include "translations/$language.php";
 
 ?><!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title><?php echo $translations['title'] ?></title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/style.css" rel="stylesheet">
     <style>
 body {
     font-family: 'Helvetica Neue', Arial, sans-serif;
