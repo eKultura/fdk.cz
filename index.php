636b36db75c854d9998273aad951aa514b9021fa
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-05 21:15:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
+<?php
+
+require_once "include/db_connetion.php";
+include "includes/settings.php";
+
+$translations = include "translations/$language.php";
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Správce úkolů</title>
+    <title><?php echo $translations['title'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
 body {
