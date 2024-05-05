ac0ecbc51cf8ed801cbc64cdf2c05f05c89244d5
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-05 21:36:38 +0200

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -5,13 +5,11 @@ $username = "";
 $password = "";
 $dbname = "";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check if the connection is successful
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+   // echo "Connection failed: " . $e->getMessage();
 }
-
 
 ?>
