88242185b47d55936b5dd552394619148998f7db
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:31:54 +0200

    Create fr.php

---
+++ assets/translations/fr.php
@@ -0,0 +1,32 @@
+<?php
+
+    return [
+    'about_us' => 'À propos de nous',  
+    'project' => 'projet',
+    'projects' => 'projets',
+    'task' => 'tâche',
+    'tasks' => 'tâches',
+
+    'to_do' => 'À faire',
+    'in_progress' => 'En cours',
+    'done' => 'Terminé',
+    
+    'index_title' => 'Gestionnaire de tâches',
+    'index_sidebar_text' => 'Un projet destiné aux besoins d’une équipe unique, 
+            mais ouvert aux tests pour tous.',
+    'index_text' => 'Bienvenue dans notre outil révolutionnaire de gestion 
+            des tâches! Notre gestionnaire de tâches offre un moyen rapide 
+            et flexible de coordonner le travail de votre équipe. 
+            Ce qui n’était auparavant qu’un rêve devient maintenant une réalité. 
+            Et c’est en plus totalement gratuit! Oubliez les frontières de la 
+            collaboration et propulsez votre équipe dans une nouvelle ère de 
+            productivité.', 
+
+    'footer_about_us' => 'À propos de nous',    
+    'footer_text' => 'Lorem ipsum dolor sit amet. 
+            Oui, nous voulons ce texte ici. 
+            C&apos;est un symbole de quelque chose qui émerge et change. 
+            La seule constante dans la vie est le changement.',
+];
+
+?>
