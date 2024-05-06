c67eb19084c1c09f402332f79ae16d52878f36fb
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:31:08 +0200

    Update en.php

--- assets/translations/en.php
+++ assets/translations/en.php
@@ -1,8 +1,23 @@
 <?php
 return [
+    'about_us' => 'About Us',
     'project' => 'project',
     'projects' => 'projects',
     'task' => 'task',
     'tasks' => 'tasks',
+
+    'to_do' => 'To-Do',
+    'in_progress' => 'In Progress',
+    'done' => 'Done',
+
+    'index_title' => 'Task Manager',
+    'index_sidebar_text' => 'A project designed for the needs of a unique team, yet open for testing to everyone.',
+    'index_text' => 'Welcome to the revolutionary task management tool! Our task manager brings a fast and flexible way to coordinate your team&apos;s work. What was once just a dream is now becoming a reality. And it&apos;s available completely free of charge! Forget the boundaries of collaboration and move your team into a new era of productivity.',
+    
+    'footer_about_us' => 'About Us',
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Yes, we want this text here.
+                    It is a symbol of something that is being created and changing.
+                    The only constant in life is change.',
 ];
 ?>
