4a905b38cbbc1868644dcddc686d128c206bff95
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:37:51 +0200

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -1,3 +1,11 @@
 <?php
-$language = 'cz'; 
+
+$language = 'cs';
+
+include './assets/languages.php';
+
+if (isset($_GET['lang']) && array_search($_GET['lang'], $languages) !== false) {
+    $language = $_GET['lang'];
+}
+
 ?>
