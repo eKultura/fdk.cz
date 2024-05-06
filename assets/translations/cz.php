e60da792c1da1e6f9889c0b5b39cf00706656f9e
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:30:24 +0200

    Update cz.php

--- assets/translations/cz.php
+++ assets/translations/cz.php
@@ -1,9 +1,24 @@
 <?php
 return [
+    'about_us' => 'O nás',  
     'project' => 'projekt',
     'projects' => 'projekty',
     'task' => 'úkol',
     'tasks' => 'úkoly',
+
+    'to_do' => 'Ke zpracování',
+    'in_progress' => 'Probíhá',
+    'done' => 'Hotovo',
+    
     'index_title' => 'Správce úkolů',
+    'index_sidebar_text' => 'Projekt pro potřeby jednoho unikátního týmu, ale otevřeno pro testování všem.',
+    'index_text' => 'Vítejte v revolučním nástroji pro správu úkolů! Náš task manager přináší rychlý a pružný způsob, jak koordinovat práci Vašeho týmu. To, co bylo dříve pouze snem, se nyní stává skutečností. A je navíc k dispozici zcela zdarma! Zapomeňte na hranice spolupráce a přesuňte svůj tým do nové éry produktivity.', 
+
+    'footer_about_us' => 'O nás',    
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Ano, tento text tu chceme mít. 
+                    Je to symbol něčeho co vzniká a mění se. 
+                    Jediná konstanta v životě je změna.',
+    
 ];
 ?>
