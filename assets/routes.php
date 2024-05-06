be08c87bf2e20f55fc2625314c7a8c2a37c6c9a4
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 16:17:34 +0200

    Create routes.php

---
+++ assets/routes.php
@@ -0,0 +1,96 @@
+<?php
+// routes.php
+
+return [
+    'cs' => [
+        'prihlaseni' => 'login.php',
+        'registrace' => 'registration.php',
+        'novy-projekt' => 'new-project.php',
+        'novy-ukol' => 'new-task.php',
+        
+
+    ],
+    'en' => [
+        'login' => 'login.php',
+        'registration' => 'registration.php',
+        'new-project' => 'new-project.php',
+        'new-task' => 'new-task.php',
+    ],
+    'sk' => [
+        'prihlasenie' => 'login.php',
+        'registracia' => 'registration.php',
+        'novy-projekt' => 'new-project.php',
+        'novy-ukol' => 'new-task.php',
+    ],
+    'de' => [
+        'anmeldung' => 'login.php',
+        'registrierung' => 'registration.php',
+        'neues-projekt' => 'new-project.php',
+        'neue-aufgabe' => 'new-task.php',
+    ],
+    'es' => [
+        'inicio-sesion' => 'login.php',
+        'registro' => 'registration.php',
+        'nuevo-proyecto' => 'new-project.php',
+        'nueva-tarea' => 'new-task.php',
+    ],
+    'pl' => [
+        'logowanie' => 'login.php',
+        'rejestracja' => 'registration.php',
+        'nowy-projekt' => 'new-project.php',
+        'nowe-zadanie' => 'new-task.php',
+    ],
+    'fr' => [
+        'connexion' => 'login.php',
+        'enregistrement' => 'registration.php',
+        'nouveau-projet' => 'new-project.php',
+        'nouvelle-tache' => 'new-task.php',
+    ],
+    'it' => [
+        'accesso' => 'login.php',
+        'registrazione' => 'registration.php',
+        'nuovo-progetto' => 'new-project.php',
+        'nuovo-compito' => 'new-task.php',
+    ],
+    'ru' => [
+        'vhod' => 'login.php',
+        'registratsiya' => 'registration.php',
+        'novyi-proekt' => 'new-project.php',
+        'novoe-zadanie' => 'new-task.php',
+    ],
+    'pt' => [
+        'login' => 'login.php',
+        'registro' => 'registration.php',
+        'novo-projeto' => 'new-project.php',
+        'nova-tarefa' => 'new-task.php',
+    ],
+    'nl' => [
+        'inloggen' => 'login.php',
+        'registratie' => 'registration.php',
+        'nieuw-project' => 'new-project.php',
+        'nieuwe-taak' => 'new-task.php',
+    ],
+    'zh' => [
+        'denglu' => 'login.php',
+        'zhuce' => 'registration.php',
+        'xin-projekt' => 'new-project.php',
+        'xin-renwu' => 'new-task.php',
+    ],
+    'ja' => [
+        'roguin' => 'login.php',
+        'kayot' => 'registration.php',
+        'shin-projekuto' => 'new-project.php',
+        'shin-kodomo' => 'new-task.php',
+    ],
+    'ko' => [
+        'id' => 'login.php',
+        'gangeum' => 'registration.php',
+        'saeroun peurojegteu' => 'new-project.php',
+        'saeroun ili' => 'new-task.php',
+    ],
+
+
+
+];
+
+?>
