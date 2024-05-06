793e3dbd6f4b829f23c1f9c2a8dde558c93c35f6
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:31:29 +0200

    Update sk.php

--- assets/translations/sk.php
+++ assets/translations/sk.php
@@ -1,8 +1,23 @@
 <?php
 return [
+    'about_us' => 'O nás',
     'project' => 'projekt',
     'projects' => 'projekty',
     'task' => 'úloha',
     'tasks' => 'úlohy',
+
+    'to_do' => ' Na spracovanie',
+    'in_progress' => 'Prebieha',
+    'done' => 'Hotovo',
+
+    'index_title' => 'Manažér úloh',
+    'index_sidebar_text' => 'Projekt určený pre potreby jedinečného tímu, ale otvorený pre testovanie všetkým.',
+    'index_text' => 'Vitajte v revolučnom nástroji na správu úloh! Náš správca úloh prináša rýchly a flexibilný spôsob, ako koordinovať prácu vášho tímu. To, čo bolo predtým len snom, sa teraz stáva skutočnosťou. Navyše je k dispozícii úplne zadarmo! Zabudnite na hranice spolupráce a posuňte svoj tím do novej éry produktivity.', 
+    
+    'footer_about_us' => 'O nás',
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Áno, tento text tu chceme mať.
+                    Je to symbol niečoho, čo vzniká a mení sa.
+                    Jediná konštanta v živote je zmena.',
 ];
 ?>
