8ec5dacb8218c15ab10ba2ed603aa7b425b7eff0
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:35:41 +0200

    Update languages.php

--- assets/languages.php
+++ assets/languages.php
@@ -4,5 +4,19 @@ $languages = array(
     'czech' => 'cs',
     'english' => 'en',
     'slovakian' => 'sk',
+    'german' => 'de',
+    'spanish' => 'es',
+    'polish' => 'pl',
+    
+    'french' => 'fr',
+    'italian' => 'it',
+    'russian' => 'ru',
+    'portuguese' => 'pt',
+    'dutch' => 'nl',
+    'chinese' => 'zh',
+    'japanese' => 'ja',
+    'korean' => 'ko'
 );
+
+
 ?>
