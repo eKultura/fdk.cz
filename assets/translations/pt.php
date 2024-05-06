d629a14f6a0752d1480ad7d381fe9aeb6105e314
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:32:51 +0200

    Create pt.php

---
+++ assets/translations/pt.php
@@ -0,0 +1,26 @@
+<?php
+
+    return [
+    'about_us' => 'Sobre nós',  
+    'project' => 'projeto',
+    'projects' => 'projetos',
+    'task' => 'tarefa',
+    'tasks' => 'tarefas',
+
+    'to_do' => 'A fazer',
+    'in_progress' => 'Em andamento',
+    'done' => 'Concluído',
+    
+    'index_title' => 'Gestor de tarefas',
+    'index_sidebar_text' => 'Um projeto desenhado para as necessidades de uma equipe única, mas aberto para testes por todos.',
+    'index_text' => 'Bem-vindo ao nosso revolucionário gerenciador de tarefas! Nosso task manager oferece uma forma rápida e flexível de coordenar o trabalho da sua equipe. O que antes era apenas um sonho, agora se torna realidade. E está disponível totalmente grátis! Esqueça os limites da colaboração e leve sua equipe para uma nova era de produtividade.', 
+
+    'footer_about_us' => 'Sobre nós',    
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Sim, queremos ter este texto aqui.
+                    É um símbolo de algo que surge e muda.
+                    A única constante na vida é a mudança.',
+];
+
+
+?>
