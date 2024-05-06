b159d7177cb91115425031bf7d62382feca4e5a7
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:36:53 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,24 @@
+
     <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <h4>O nás</h4>
-                    <p>Lorem ipsum dolor sit amet. Ano, tento text tu chceme mít. Je to symbol něčeho co vzniká a mění se. </p>
+                    <h4><?php echo $translations['about_us'] ?></h4>
+                    <p>
+                    <?php echo $translations['footer_text'] ?>
+                    </p>
                 </div>
                 <div class="col-md-4">
                     <h4>Užitečné odkazy</h4>
                     <ul class="footer-links">
-                        <li><a href="https://fdk.cz">Domů</a></li>
-                        <li><a href="#">O nás</a></li>
-                        <li><a href="#">Služby</a></li>
-                        <li><a href="#">Kontakt</a></li>
+                        <li>🔗 <a href="/">Domů</a></li>
+                        <li>🔗 <a href="/">O nás</a></li>
+                        <li>🔗 <a href="/">Služby</a></li>
+                        <li>🔗 <a href="/#contact">Kontakt</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    <h4>Kontaktuje nás:</h4> 
+                    <h4 id="contact">Kontaktuje nás:</h4> 
                     <p>eKultura z.s.</p>
                     <p>Email: info@div.cz</p>
                     <p>Phone: hmm, uvidíme</p>
@@ -23,3 +26,6 @@
             </div>
         </div>
     </footer>
+
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
