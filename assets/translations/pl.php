f46b0d932472e3b728f64d44feefe76ad33ee12a
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:35:07 +0200

    Create pl.php

---
+++ assets/translations/pl.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'about_us' => 'O nas',
+    'project' => 'Projekt',
+    'projects' => 'Projekty',
+    'task' => 'Zadanie',
+    'tasks' => 'Zadania',
+
+    'to_do' => 'Do przetworzenia',
+    'in_progress' => 'W trakcie',
+    'done' => 'Gotowe',
+    
+    'index_title' => 'Menadżer zadań',
+    'index_sidebar_text' => 'Projekt zaprojektowany dla potrzeb unikalnego zespołu, ale otwarty na testowanie dla wszystkich.',
+    'index_text' => 'Witaj w rewolucyjnym narzędziu do zarządzania zadaniami! Nasz menedżer zadań oferuje szybki i elastyczny sposób koordynowania pracy Twojego zespołu. To, co kiedyś było tylko marzeniem, teraz staje się rzeczywistością. Co więcej, jest dostępne całkowicie za darmo! Zapomnij o granicach współpracy i przenieś swój zespół do nowej ery produktywności.', 
+    
+    'footer_about_us' => 'O nas',
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Tak, chcemy tutaj ten tekst. 
+                    To symbol czegoś, co powstaje i zmienia się. 
+                    Jedyną stałą w życiu jest zmiana.',
+];
+?>
