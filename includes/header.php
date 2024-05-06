c5263ef943498113d0156a7dd35069e749b6de50
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:40:19 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
     <header class="bg-light py-4">
         <div class="container text-center">
-            <h1 class="mb-0">Správce úkolů</h1>
+            <h1 class="mb-0"><?php echo $translations['index_title'] ?></h1>
         </div>
     </header>
