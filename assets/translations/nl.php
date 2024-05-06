dfaeb80c7bf90df3887bc396299cb45bb75e29c9
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:33:10 +0200

    Create nl.php

---
+++ assets/translations/nl.php
@@ -0,0 +1,27 @@
+<?php
+
+    return [
+    'about_us' => 'Over ons',  
+    'project' => 'project',
+    'projects' => 'projecten',
+    'task' => 'taak',
+    'tasks' => 'taken',
+
+    'to_do' => 'Te doen',
+    'in_progress' => 'Bezig',
+    'done' => 'Gedaan',
+    
+    'index_title' => 'Taakbeheerder',
+    'index_sidebar_text' => 'Een project ontworpen voor de behoeften van een uniek team, maar open voor iedereen om te testen.',
+    'index_text' => 'Welkom bij onze revolutionaire taakbeheerder! Onze taakmanager biedt een snelle en flexibele manier om het werk van uw team te coördineren. Wat ooit slechts een droom was, wordt nu werkelijkheid. En het is volledig gratis beschikbaar! Vergeet de grenzen van samenwerking en verplaats uw team naar een nieuw tijdperk van productiviteit.', 
+
+    'footer_about_us' => 'Over ons',    
+    'footer_text' => '
+                    Lorem ipsum dolor sit amet. Ja, we willen deze tekst hier hebben.
+                    Het is een symbool van iets dat ontstaat en verandert.
+                    De enige constante in het leven is verandering.',
+];
+
+
+
+?>
