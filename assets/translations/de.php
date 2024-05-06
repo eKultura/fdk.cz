55252ed0d320d89e86416707628895579cba6b23
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:34:28 +0200

    Create de.php

---
+++ assets/translations/de.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'about_us' => 'Über uns',
+    'project' => 'Projekt',
+    'projects' => 'Projekte',
+    'task' => 'Aufgabe',
+    'tasks' => 'Aufgaben',
+
+    'to_do' => 'Zur Bearbeitung',
+    'in_progress' => 'In Bearbeitung',
+    'done' => 'Erledigt',
+    
+    'index_title' => 'Aufgabenmanager',
+    'index_sidebar_text' => 'Ein Projekt für die Bedürfnisse eines einzigartigen Teams, aber offen für Tests für alle.',
+    'index_text' => 'Willkommen beim revolutionären Werkzeug zur Aufgabenverwaltung! Unser Task-Manager bietet eine schnelle und flexible Möglichkeit, die Arbeit Ihres Teams zu koordinieren. Was einst nur ein Traum war, wird jetzt Wirklichkeit. Und das völlig kostenlos! Vergessen Sie die Grenzen der Zusammenarbeit und führen Sie Ihr Team in eine neue Ära der Produktivität.', 
+    
+    'footer_about_us' => 'Über uns',
+    'footer_text' => 'Lorem ipsum dolor sit amet. Ja, diesen Text wollen wir hier haben. Es ist das Symbol für etwas, das entsteht und sich verändert. Die einzige Konstante im Leben ist der Wandel.',
+];
+?>
