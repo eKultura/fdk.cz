8714fb197f5341dbce9483aa41b19fd1206285b0
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-06 13:32:16 +0200

    Create it.php

---
+++ assets/translations/it.php
@@ -0,0 +1,30 @@
+<?php
+
+    return [
+    'about_us' => 'Chi siamo',  
+    'project' => 'progetto',
+    'projects' => 'progetti',
+    'task' => 'compito',
+    'tasks' => 'compiti',
+
+    'to_do' => 'Da fare',
+    'in_progress' => 'In corso',
+    'done' => 'Fatto',
+    
+    'index_title' => 'Gestore di compiti',
+    'index_sidebar_text' => 'Un progetto per le esigenze di un team unico, 
+            ma aperto ai test per tutti.',
+    'index_text' => 'Benvenuti nel nostro strumento rivoluzionario per la 
+            gestione dei compiti! Il nostro task manager offre un modo rapido e 
+            flessibile per coordinare il lavoro del tuo team. Quello che prima 
+            era solo un sogno ora diventa realtà. Ed è inoltre disponibile 
+            completamente gratis! Dimentica i limiti della collaborazione e sposta 
+            il tuo team in una nuova era di produttività.', 
+
+    'footer_about_us' => 'Chi siamo',    
+    'footer_text' => 'Lorem ipsum dolor sit amet. Sì, vogliamo questo testo qui. 
+            È un simbolo di qualcosa che nasce e cambia. L’unica costante nella vita è 
+            il cambiamento.',
+];
+
+?>
