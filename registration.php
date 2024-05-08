94781e144d129e890c54a8047c036ba5e7d1b82c
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-08 17:16:59 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -16,8 +16,8 @@ if (isset($_GET['lang']) && array_search($_GET['lang'], $languages) !== false) {
     $language = $_GET['lang'];
 }
 ### ### ###  ### ### ###  ### ### ###
-### ### ###  / INCLUDES   ### ### ###
-### ### ###  ### ### ###  ### ### ###
+### ### ###  / Universal  ### ### ###
+### ### ###  / includes   ### ### ###
 
 
 $name = null;
