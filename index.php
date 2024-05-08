00a973f3f8e2d635593d5d831680fd82c5df3cc9
3s7an <tristan.prekop13@gmail.com>
2024-05-08 09:30:45 +0200

    added navigation

--- index.php
+++ index.php
@@ -22,6 +22,34 @@ $translations = include "./assets/translations/$language.php";
 
 <?php include "includes/header.php"; ?>
 
+<!-- Navigation -->
+<nav class="navbar border-bottom">
+<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+           Logged user : 
+          </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">My profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">My tasks</a>
+              </li>
+              <form class="d-flex mt-3" method="post">
+                <button>Log out</button>
+            </form>
+        </div>
+        </div>
+
+
+<!-- end of navigation -->
 
 <div class="container mt-5">
     <div class="row">
