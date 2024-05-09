222c54f26bd3501532f34602cb0c1ea08815353d
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-05-09 11:08:20 +0200

    Update cs.php

--- assets/translations/cs.php
+++ assets/translations/cs.php
@@ -12,13 +12,49 @@ return [
     
     'index_title' => 'Správce úkolů',
     'index_sidebar_text' => 'Projekt pro potřeby jednoho unikátního týmu, ale otevřeno pro testování všem.',
-    'index_text' => 'Vítejte v revolučním nástroji pro správu úkolů! Náš task manager přináší rychlý a pružný způsob, jak koordinovat práci Vašeho týmu. To, co bylo dříve pouze snem, se nyní stává skutečností. A je navíc k dispozici zcela zdarma! Zapomeňte na hranice spolupráce a přesuňte svůj tým do nové éry produktivity.', 
+    'index_text' => 'Vítejte v revolučním nástroji pro správu úkolů! Náš task manager přináší rychlý a pružný způsob, 
+                     jak koordinovat práci Vašeho týmu. To, co bylo dříve pouze snem, se nyní stává skutečností. 
+                     A je navíc k dispozici zcela zdarma! Zapomeňte na hranice spolupráce a přesuňte svůj tým do nové éry produktivity.', 
 
     'footer_about_us' => 'O nás',    
     'footer_text' => '
                     Lorem ipsum dolor sit amet. Ano, tento text tu chceme mít. 
                     Je to symbol něčeho co vzniká a mění se. 
                     Jediná konstanta v životě je změna.',
+    
+    
+    'new_project_left_sidebar' => 'Každý projekt má mít svoji doménu. Pokud si doménu ověříte, projekt bude žít navěky. Pro neověřené domeny má projekt platnost 1 měsíc.',
+    'new_project_form_title' => 'Vytvořit nový projekt',
+    'new_project_form_name' => 'Název projektu nebo produktu',
+    'new_project_form_url' => 'URL (ideálně doména projektu)',
+    'new_project_form_description' => 'Popis projektu',
+    'new_project_form_checkbox' => 'Veřejný projekt',
+    'new_project_form_checkbox_title' => 'Každý uvidí obsah, včetně úkolů, které jsou přiřazeny ostatním.',
+    'new_project_form_button' => 'Vytvořit projekt', 
+
+
+    'registration_page_title' => 'Registrace',
+    'registration_sidebar_text' => 'Registrace vám umožní spravovat všechny svoje projekty a přidávat další členy do týmu.',
+    'registration_form_sign_in' => 'Zaregistrovat se',
+    'registration_form_email' => 'Email',
+    'registration_form_name' => 'Jméno',
+    'registration_form_password' => 'Heslo',
+    'registration_form_create_account' => 'Vytvořit účet',
+    'registration_form_already_have_account_text' => 'Už máš účet? Přihlaš se ->', //Already have account? Let´s sign in ->
+    'registration_error_registraton' => 'Registrace se nezdařila.',
+    'registration_error_user_exist' => 'Uživatelské jméno nebo e-mail již existuje.',
+
+
+    'sign_in_page_title' => 'Přihlášení na fdk.cz',  
+    'sign_in_page_title' => 'Přihlášení na fdk.cz',  
+    'sign_in_sidebar_text' => 'Přihlášení vám umožní spravovat všechny svoje projekty a přidávat další členy do týmu.',
+    'sign_in_error_user_not_found' => 'Uživatel s těmito údaji nenalezen.',
+    'sign_in_error_incorrect_login_information' => 'Nesprávné údaje. Zkus prosím znovu.',
+    'sign_in_form_name' => 'Jméno',
+    'sign_in_form_password' => 'Heslo',
+    'sign_in_form_button' => 'Přihlásit',  
+    'sign_in_form_new_user' => 'Nový uživatel? Rychlá registrace ->', //New user? Let´s create an account ->
+ 
     
 ];
 ?>
