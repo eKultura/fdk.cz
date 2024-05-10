21f10df865baf9153af6c710ea35e1dc8416fd41
3s7an <tristan.prekop13@gmail.com>
2024-05-10 09:07:37 +0200

    navbar

--- index.php
+++ index.php
@@ -44,16 +44,60 @@ if (isset($_SESSION["username"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title><?php echo $translations['index_title'] ?></title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <link href="https://fdk.cz/assets/style.css" rel="stylesheet">
 
 </head>
 <body>
 
 <?php include "includes/header.php"; ?>
+
+<!-- NAVBAR -->
+<nav class="navbar bg-body-tertiary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Správce úkolů</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                <?php if(array_key_exists('username', $_SESSION)){
+                echo "Logged user $username";
+            }
+            else {
+                echo "<a>Login</a>";
+            }
+            
+            ?>
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Můj účet</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Moje projekty</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#">Moje úkoly</a>
+            </li>
+              
+               
+            <form class="d-flex mt-3" role="search">
+             <button class="btn btn-outline-danger" type="submit" name="logout">Logout</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
 
 
 <div class="container mt-5">
