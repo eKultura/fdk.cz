d07cdae5d4b297beb924bb93cc68a26116a48bf2
3s7an <tristan.prekop13@gmail.com>
2024-05-13 12:57:56 +0200

    prihlaseni zmena

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-    <header class="bg-light py-4 mt-5">
+    <header class="bg-light py-4">
         <div class="container text-center">
             <h1 class="mb-0"><?php echo $translations['index_title'] ?></h1>
         </div>
--- prihlaseni.php
+++ prihlaseni.php
@@ -70,6 +70,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
     <title><?php echo $translations['sign_in_page_title'] ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <link href="https://fdk.cz/assets/style.css" rel="stylesheet">
@@ -96,9 +97,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
         <div class="col-md-9 col-12">
 
     <main class="form-signin w-100 m-auto text-center mt-1 mb-5">
-
-
-            <h1 class="h3 mb-3 fw-normal">Přihlášení</h1>
+    
+    <!--POP OVER -->
+    <span class="d-inline-block text-dark" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="<?php echo $translations['sign_in_sidebar_text'] ?>">
+      <button class="btn border border-white" type="button" disabled=""><h1 class="text-dark">Prihlašení </h1></button>
+    </span>
 
                 <?php if (!isset($_SESSION["username"])): ?>
         <form method="post">
@@ -132,6 +135,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
 
 
     <?php include "./includes/footer.php" ?>
+
+    <script src="tooltips.js"></script>
 
 
 
